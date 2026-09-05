dfabfbfb93146fd172b35dbb925a6535a572c7c0
Gianni <gianni.dev.morana@gmail.com>
2026-09-05 17:42:17 +0200

    checked task and deleted task added

--- taskItem.js
+++ taskItem.js
@@ -8,28 +8,33 @@ function addTaskItem() {
   newInput.value = "";
 }
 
-function deleteTaskItem() {
-  // TODO:
-  // [] ...
-  // ... do logic here
+function deleteTaskItem(e) {
+  const taskItem = e.target.closest(".task-item");
+  taskItem.remove();
 }
 
-function checkTaskItem() {
-  // ... do logic here
+function checkTaskItem(e) {
+  if (e.target.checked) {
+    e.target.nextSibling.classList.add("task-text-checkboxed");
+  } else {
+    e.target.nextSibling.classList.remove("task-text-checkboxed");
+  }
 }
 
 function createTaskItem(taskName) {
   const task = createElementWithClass("div", "task-item");
   const content = createElementWithClass("div", "content");
-  const input = document.createElement("input");
-  input.setAttribute("type", "checkbox");
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.addEventListener("click", checkTaskItem);
   const text = createElementWithClass("div", "task-text");
   text.innerText = taskName;
   const deleteButton = createElementWithClass("button", "delete-button");
   const closeIcon = createElementWithClass("span", "material-symbols-outlined");
   closeIcon.innerText = "close";
+  deleteButton.addEventListener("click", deleteTaskItem);
   task.appendChild(content);
-  content.appendChild(input);
+  content.appendChild(checkbox);
   content.appendChild(text);
   task.appendChild(deleteButton);
   deleteButton.appendChild(closeIcon);
